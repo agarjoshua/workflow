18f116ac98433101044dbe4aaeb17949a461f73f
agar joshua <48643686+agarjoshua@users.noreply.github.com>
2020-06-14 12:24:44 +0300

    Add files via upload

---
+++ workflow.py
@@ -0,0 +1,23 @@
+
+
+# Imports for selective yt playlist
+from selenium import webdriver
+from getpass import getpass
+from webdriver_manager.chrome import ChromeDriverManager
+# Imports for internal subprocesses
+import subprocess
+
+
+# Script that opens my favourite youtube playlist
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get('https://www.youtube.com/playlist?list=PLvNfwQlwCdEpfHOZWckGcJ76E_wSKRK8S')
+
+driver = driver.find_element_by_css_selector('#img')
+driver.click()
+
+
+# this section opens my vscode workstation and last project
+input('press y to open a new vs window')
+
+subprocess.Popen('C:\\Users\\AGAR\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe')
